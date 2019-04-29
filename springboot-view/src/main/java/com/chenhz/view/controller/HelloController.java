294a1ee22820dc06981a466b67fd43d3803c7f5c
chenhouzhen <chenhouzhen@hqjy.com>
2019-04-29 17:13:22 +0800

    玩前端

--- springboot-view/src/main/java/com/chenhz/view/controller/HelloController.java
+++ springboot-view/src/main/java/com/chenhz/view/controller/HelloController.java
@@ -1,5 +1,7 @@
 package com.chenhz.view.controller;
 
+import com.chenhz.common.entity.R;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,5 +9,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("hello")
 public class HelloController {
 
+    @GetMapping("/say")
+    public R say(String name){
+        return R.ok("hi!"+ name);
+    }
 
 }
