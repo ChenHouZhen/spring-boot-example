2b74009247ea029dc92d0e49b045eff96d469b00
chenhz <843207296@qq.com>
2019-05-04 17:30:27 +0800

    增加

--- springboot-blog/src/main/java/com/chenhz/blog/controller/SysLoginController.java
+++ springboot-blog/src/main/java/com/chenhz/blog/controller/SysLoginController.java
@@ -1,10 +1,15 @@
 package com.chenhz.blog.controller;
 
 import com.chenhz.blog.shiro.ShiroUtils;
+import com.chenhz.common.entity.R;
 import com.google.code.kaptcha.Constants;
 import com.google.code.kaptcha.Producer;
+import org.apache.shiro.authc.*;
+import org.apache.shiro.subject.Subject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.imageio.ImageIO;
@@ -38,5 +43,42 @@ public class SysLoginController {
 
         ServletOutputStream out = response.getOutputStream();
         ImageIO.write(image, "jpg", out);
+    }
+
+    /**
+     * 登录
+     */
+    @ResponseBody
+    @RequestMapping(value = "/sys/login", method = RequestMethod.POST)
+    public R login(String username, String password, String captcha) {
+        String kaptcha = ShiroUtils.getKaptcha(Constants.KAPTCHA_SESSION_KEY);
+        if(!captcha.equalsIgnoreCase(kaptcha)){
+            return R.error("验证码不正确");
+        }
+
+        try{
+            Subject subject = ShiroUtils.getSubject();
+            UsernamePasswordToken token = new UsernamePasswordToken(username, password);
+            subject.login(token);
+        }catch (UnknownAccountException e) {
+            return R.error(e.getMessage());
+        }catch (IncorrectCredentialsException e) {
+            return R.error("账号或密码不正确");
+        }catch (LockedAccountException e) {
+            return R.error("账号已被锁定,请联系管理员");
+        }catch (AuthenticationException e) {
+            return R.error("账户验证失败");
+        }
+
+        return R.ok();
+    }
+
+    /**
+     * 退出
+     */
+    @RequestMapping(value = "logout", method = RequestMethod.GET)
+    public String logout() {
+        ShiroUtils.logout();
+        return "redirect:login.html";
     }
 }
